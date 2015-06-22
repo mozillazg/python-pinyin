a50950d7815f935c43d65f37da2ac303a4cc0707
mozillazg <opensource.mozillazg@gmail.com>
2015-06-22 12:43:01 +0800

    more tests

--- tests/test_others.py
+++ tests/test_others.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 from pypinyin import *   # noqa
-from .utils import has_module
+from pypinyin import simple_seg
 
 
 def test_import_all():
@@ -11,3 +11,13 @@ def test_import_all():
     pinyin('啦啦啦', TONE2)
     lazy_pinyin('啦啦啦')
     slug('啦啦啦')
+
+
+def test_simple_seg():
+    assert simple_seg('啦啦') == ['啦啦']
+    assert simple_seg('啦啦abc') == ['啦啦', 'abc']
+    assert simple_seg('&##啦啦abc') == ['&##', '啦啦', 'abc']
+    assert simple_seg('&#哦#啦啦abc') == ['&#', '哦', '#', '啦啦', 'abc']
+    assert simple_seg('哦ほ#') == ['哦', 'ほ#']
+    assert simple_seg(['啦啦']) == ['啦啦']
+    assert simple_seg(['啦啦', 'abc']) == ['啦啦', 'abc']
