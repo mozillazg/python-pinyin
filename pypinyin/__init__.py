f87e1a5f349c39eda2427dd451f92d7868163763
mozillazg <opensource.mozillazg@gmail.com>
2016-10-19 21:44:56 +0800

    Bump version: 0.14.0 → 0.15.0

--- pypinyin/__init__.py
+++ pypinyin/__init__.py
@@ -19,7 +19,7 @@ from .core import (     # noqa
 )
 
 __title__ = 'pypinyin'
-__version__ = '0.14.0'
+__version__ = '0.15.0'
 __author__ = 'mozillazg, 闲耘'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2016 mozillazg, 闲耘'
