5aae89ecaa133928f78117b2540fed78cb2fbd42
mozillazg <mozillazg101@gmail.com>
2017-03-12 18:17:53 +0800

    增加几处注释

--- gen_pinyin_dict.py
+++ gen_pinyin_dict.py
@@ -10,23 +10,26 @@ from __future__ import unicode_literals
 pinyin_dict = {
 ''')
     for line in in_fp.readlines():
-        if (line.startswith('"') or line.startswith('#') or
-                not line.strip()):
+        line = line.strip()
+        if line.startswith('#') or not line:
             continue
         else:
             # line is U+4E2D: zhōng,zhòng  # 中
             # raw_line U+4E2D: zhōng,zhòng
             raw_line = line.split('#')[0].strip()
+            # 0x4E2D: zhōng,zhòng
             new_line = raw_line.replace('U+', '0x')
-            new_line = new_line.replace(': ', ": '") + "',"
-            #     0x4E2D: 'zhōng,zhòng'
-            out_fp.write(' ' * 4 + new_line + '\n')
+            # 0x4E2D: 'zhōng,zhòng
+            new_line = new_line.replace(': ', ": '")
+            #     0x4E2D: 'zhōng,zhòng'\n
+            new_line = "    {new_line}',\n".format(new_line=new_line)
+            out_fp.write(new_line)
     out_fp.write('}\n')
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        print('python gen.py INPUT OUTPUT')
+        print('python gen_pinyin_dict.py INPUT OUTPUT')
         sys.exit(1)
     in_f = sys.argv[1]
     out_f = sys.argv[2]
