8c5b6d1104f67774b9d5fab30e7700ff1a30b814
mozillazg <mozillazg101@gmail.com>
2019-10-20 10:38:10 +0800

    Add more tests for NeutralToneWith5Mixin

--- tests/contrib/test_neutral_tone.py
+++ tests/contrib/test_neutral_tone.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from pytest import mark
+
 from pypinyin import lazy_pinyin, Style
 from pypinyin.contrib.neutral_tone import NeutralToneWith5Mixin
 from pypinyin.contrib.uv import V2UMixin
@@ -28,3 +30,15 @@ def test_neutral_tone_with_5():
     assert lazy_pinyin('好了') == ['hao', 'le']
     assert my_pinyin.lazy_pinyin('好了') == ['hao', 'le']
     assert her_pinyin.lazy_pinyin('好了') == ['hao', 'le']
+
+
+@mark.parametrize('input,expected_old, expected_new', [
+    ['你好', ['ni3', 'ha3o'], ['ni3', 'ha3o']],
+    ['男孩儿', ['na2n', 'ha2i', 'er'], ['na2n', 'ha2i', 'e5r']],
+    ['我们', ['wo3', 'men'], ['wo3', 'me5n']],
+    ['衣裳', ['yi1', 'shang'], ['yi1', 'sha5ng']],
+    ['好吧', ['ha3o', 'ba'], ['ha3o', 'ba5']],
+])
+def test_neutral_tone_with_5_many_cases(input, expected_old, expected_new):
+    assert lazy_pinyin(input, style=Style.TONE2) == expected_old
+    assert my_pinyin.lazy_pinyin(input, style=Style.TONE2) == expected_new
