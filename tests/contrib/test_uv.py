6c413ca13f99801ed4b9949b8041280116b0cd39
mozillazg <mozillazg101@gmail.com>
2021-10-05 17:40:28 +0800

    lazy_pinyin 函数增加参数 `tone_sandhi` 用于控制是否对拼音结果按变调规则进行处理
    https://en.wikipedia.org/wiki/Standard_Chinese_phonology#Tone_sandhi
    
    Closes #161

--- tests/contrib/test_uv.py
+++ tests/contrib/test_uv.py
@@ -18,3 +18,6 @@ def test_v2u():
     assert lazy_pinyin('战略') == ['zhan', 'lve']
     assert my_pinyin.lazy_pinyin('战略') == ['zhan', 'lüe']
     assert lazy_pinyin('战略', v_to_u=True) == ['zhan', 'lüe']
+
+    assert lazy_pinyin('战略', v_to_u=True, neutral_tone_with_five=True,
+                       tone_sandhi=True) == ['zhan', 'lüe']
