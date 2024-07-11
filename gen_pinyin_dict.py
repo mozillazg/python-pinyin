10843691db7c5aaf7997c40fce53f824d7806d0c
汉广 <serfend@foxmail.com>
2024-07-11 18:41:55 +0800

    perf[code-loader]prevent py-code too large #322

--- gen_pinyin_dict.py
+++ gen_pinyin_dict.py
@@ -1,38 +1,54 @@
 # -*- coding: utf-8 -*-
 import sys
+import json
+import pathlib2
 
 
-def main(in_fp, out_fp):
-    out_fp.write('''# -*- coding: utf-8 -*-
+def export_runner(code_file: str, cfg_file_name: str):
+    code = """# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import pathlib2
+import json
 
 # Warning: Auto-generated file, don't edit.
-pinyin_dict = {
-''')
-    for line in in_fp.readlines():
-        line = line.strip()
-        if line.startswith('#') or not line:
-            continue
-        else:
+db_path = pathlib2.Path(__file__).parent / '{cfg_file_name}'
+with open(db_path, "r", encoding="utf-8") as f:
+    pinyin_dict = json.loads(f.read())
+
+""".format(
+        cfg_file_name=cfg_file_name,
+    )
+    with open(code_file, "w", encoding="utf-8") as f:
+        f.write(code)
+
+
+def main(in_file: str, out_file: str):
+    items = {}
+    with open(in_file, "r", encoding="utf-8") as in_file:
+        for line in in_file.readlines():
+            line = line.strip()
+            if line.startswith("#") or not line:
+                continue
             # line is U+4E2D: zhōng,zhòng  # 中
             # raw_line U+4E2D: zhōng,zhòng
-            raw_line = line.split('#')[0].strip()
+            raw_line = line.split("#")[0].strip()
             # 0x4E2D: zhōng,zhòng
-            new_line = raw_line.replace('U+', '0x')
-            # 0x4E2D: 'zhōng,zhòng
-            new_line = new_line.replace(': ', ": '")
-            #     0x4E2D: 'zhōng,zhòng'\n
-            new_line = "    {new_line}',\n".format(new_line=new_line)
-            out_fp.write(new_line)
-    out_fp.write('}\n')
+            new_line = raw_line.replace("U+", "0x")
+            # '0x4E2D',' zhōng,zhòng'
+            new_line = new_line.split(":")
+            items[new_line[0].strip()] = new_line[1].strip()
+    db_file = f"{out_file}.json"
+    with open(db_file, "w", encoding="utf-8") as f:
+        f.write(json.dumps(items, ensure_ascii=False))
+    cfg_file_name = pathlib2.Path(db_file).name
+    export_runner(out_file, cfg_file_name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print('python gen_pinyin_dict.py INPUT OUTPUT')
+        print("python gen_pinyin_dict.py INPUT OUTPUT")
         sys.exit(1)
     in_f = sys.argv[1]
     out_f = sys.argv[2]
 
-    with open(in_f) as in_fp, open(out_f, 'w') as out_fp:
-        main(in_fp, out_fp)
+    main(in_f, out_f)
